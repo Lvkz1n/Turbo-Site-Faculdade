726c55df4c8260581cbe583a3fbfb15a972e2e8c
NoGr0und <goenjitio@gmail.com>
2023-11-25 21:51:50 -0300

    Patch Botao filtro

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,3 +66,15 @@ bannerIndicators.forEach((indicator, index) => {
 });
 
 setInterval(nextSlide, 5000);
+
+
+function filterElements(category) {
+  const elements = document.querySelectorAll('.box');
+
+  elements.forEach((element) => {
+    element.classList.remove("show");
+    if (category === "Todos" || element.classList.contains(category)) {
+      element.classList.add("show");
+    }
+  });
+}
